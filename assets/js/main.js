df2592288ef21b979a9b4671f914799bf6c19c4d
Jackie Wellons <jackie@jackie-dell.jackie>
2017-10-30 12:50:40 -0700

    add collapsing intros

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
     $(".button-collapse").sideNav();
     $('.slider').slider();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.collapsible').collapsible();
 });
 
